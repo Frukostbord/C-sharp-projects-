a265e608b3f4cd5075235b3eac48383801638fdd
Andreas <95592402+Frukostbord@users.noreply.github.com>
2023-06-14 17:17:03 +0200

    CLI square root

---
+++ Exercise2b/Exercise2b/MainProgram.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise2b
+{
+    class MainProgram
+    {
+        static void Main()
+        {
+            SquareRoot squareroot = new SquareRoot();
+            squareroot.Start();
+        }
+    }
+}
---
+++ Exercise2b/Exercise2b/Squareroot.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise2b
+{
+    class SquareRoot
+
+    {
+        private double number;
+        public void Start()
+        {
+
+            bool calcAgain = true;
+
+            //Calculate the root of anumber given by the user
+            //Continue until the user decides to exit
+
+            do
+            {
+                ReadNumber();
+                CalculateRoot();
+
+                calcAgain = CalculateAgain();
+
+            }while(calcAgain);
+
+        }
+        private void ReadNumber()
+        {
+            Console.WriteLine("Please write a valid number: ");
+
+
+            string strNumber = Console.ReadLine();
+
+            number = double.Parse(strNumber);
+
+            
+
+        }
+        private void CalculateRoot()
+        {
+            if (number >= 0)
+            {
+                double root = Math.Sqrt(number);
+                Console.WriteLine("The square root of " + number + " is: " + root);
+
+            }
+            else
+                Console.WriteLine("Negative numbers don´t have real numbers as answers!");
+
+        }
+
+        private bool CalculateAgain()
+        {
+            bool answer = false;
+            bool goodAnswer = false;   
+
+
+            Console.WriteLine();
+            Console.Write("Do you want to continue? ");
+
+            do
+            {
+                string str = Console.ReadLine();
+
+
+                str = str.ToUpper();
+
+                if (str == "Y")
+                {
+                    answer = true;
+                    goodAnswer = true;
+                }
+                else if (str == "N")
+                {
+                    answer = false;
+                    goodAnswer = true;
+                }
+                else
+                {
+                    Console.WriteLine("Please use a valid input!");
+                    goodAnswer = false;
+                }
+            }while(!goodAnswer);
+
+            return answer;
+        }
+        
+    }
+}
