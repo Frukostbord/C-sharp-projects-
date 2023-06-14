fbd9a9f4d371fa1fa61285a231ec17ebf65e2448
Andreas <95592402+Frukostbord@users.noreply.github.com>
2023-06-14 17:15:56 +0200

    Sum of numbers

---
+++ Exercise2a/Exercise2a/ForStatements.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise2a
+{
+    public class ForStatements
+    {
+        public void Start()
+        {
+            SumNumbers();
+            SumNumbersFromOneTo();
+        }
+
+        private void SumNumbers()
+        {
+            int sum = 0;
+            for (int i = 0; i <= 20; i++)
+            {
+                sum += i;
+            }
+            Console.WriteLine("The sum of 1 to 20 is : " + sum);
+        }
+        private void SumNumbersFromOneTo()
+        {
+            int endNumber = 0;
+            int sum = 0;
+
+            Console.WriteLine("Sum up numbers from 1 to x.");
+            Console.Write("Give x: ");
+            string strNum = Console.ReadLine();
+            endNumber = int.Parse(strNum);
+
+
+            for (int i = 0; i <= endNumber; i++)
+            {
+                sum += i;
+            }
+            Console.WriteLine("The sum of 1 to " + endNumber + " is : " + sum);
+        }
+    }
+}
---
+++ Exercise2a/Exercise2a/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.NetworkInformation;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise2a
+{
+    public class Program
+    {
+        public static void Main()
+        {
+            Console.Title = "Exercise2a - for statement";
+
+            ForStatements obj = new ForStatements();
+            obj.Start();
+            
+
+            Console.Write("Press enter to Exit!");
+        }
+    }
+}
