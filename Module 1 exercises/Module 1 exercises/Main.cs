6d90051d9596f7a9f813daedfe5e0f62daf5a604
Andreas <95592402+Frukostbord@users.noreply.github.com>
2023-06-14 16:58:44 +0200

    Simple CLI program

---
+++ Module 1 exercises/Module 1 exercises/Main.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Program1
+{
+    class Program
+    {
+        static void Main()
+        {
+            Products obj = new Products();
+
+            obj.Start();
+            
+        }
+    }
+}
---
+++ Module 1 exercises/Module 1 exercises/Product.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Program1
+{
+    public class Products
+    {
+        //Variables
+        private string? productname;
+        private double productprice;
+        private short productquantity;
+
+
+        public void Start()
+        {
+            //Create variables & get input
+            //Calculate
+            //Return value
+            GetName();
+            GetPrice();
+            GetQuantity();
+            ReturnValue();
+        }
+
+        private void GetName()
+        {
+            Console.WriteLine("What is the name of the product?");
+            productname = Console.ReadLine();
+         
+        }
+
+        private void GetPrice()
+        {
+            Console.WriteLine("What is the price of the product?");
+            string strprice = Console.ReadLine();
+            productprice = double.Parse(strprice);
+
+        }
+
+        private void GetQuantity()
+        {
+            Console.WriteLine("What is the quantity of the product?");
+            string strquantity = Console.ReadLine();
+            productquantity = short.Parse(strquantity);
+
+        }
+
+        public void ReturnValue()
+        {
+            Console.WriteLine("++++++++++++++++++++");
+            Console.WriteLine($"The product name is: {productname}");
+            Console.WriteLine($"The product price is: {productprice}$");
+            Console.WriteLine($"The product count is: {productquantity}");
+            Console.WriteLine("++++++++++++++++++++");
+            Console.ReadLine();
+        }
+
+    }
+}
